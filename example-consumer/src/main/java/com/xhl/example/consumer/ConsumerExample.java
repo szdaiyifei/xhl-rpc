b379ceb9528a0d148ecf3348878d55adc66ff5fb
daiyifei <2534644853@qq.com>
2024-03-16 03:02:20 +0800

    feat:支持读取 application.yml、application.yaml 等不同格式的配置文件。

--- example-consumer/src/main/java/com/xhl/example/consumer/ConsumerExample.java
+++ example-consumer/src/main/java/com/xhl/example/consumer/ConsumerExample.java
@@ -1,8 +1,14 @@
 package com.xhl.example.consumer;
 
 
+import cn.hutool.core.io.resource.ResourceUtil;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
+import cn.hutool.setting.yaml.YamlUtil;
 import com.xhl.xhlrpc.config.RpcConfig;
 import com.xhl.xhlrpc.utils.ConfigUtils;
+
+import java.util.Map;
 
 /**
  * 服务消费者实例
@@ -13,7 +19,19 @@ public class ConsumerExample {
 
     public static void main(String[] args) {
 
-        // 测试配置文件读取¬
+
+        // 加载YAML文件为Map
+        Map<String, Object> yamlMap = YamlUtil.loadByPath("application.yaml");
+//        JSONObject jsonObject =
+//                JSONUtil.parseObj(yamlMap);
+//        System.out.println(jsonObject);
+//
+//
+//        RpcConfig bean = JSONUtil.toBean(jsonObject.getJSONObject("rpc"), RpcConfig.class);
+//
+//        // 现在你可以基于yamlMap进行操作或将其转换为特定对象
+//        System.out.println(bean);
+        // 测试配置文件读取
         RpcConfig rpc = ConfigUtils.loadConfig(RpcConfig.class, "rpc");
         System.out.println(rpc);
     }
--- xhl-rpc-core/src/main/java/com/xhl/xhlrpc/utils/ConfigUtils.java
+++ xhl-rpc-core/src/main/java/com/xhl/xhlrpc/utils/ConfigUtils.java
@@ -1,7 +1,16 @@
 package com.xhl.xhlrpc.utils;
 
+import cn.hutool.core.io.FileUtil;
 import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSON;
+import cn.hutool.json.JSONConfig;
+import cn.hutool.json.JSONObject;
+import cn.hutool.json.JSONUtil;
 import cn.hutool.setting.dialect.Props;
+import cn.hutool.setting.yaml.YamlUtil;
+import com.xhl.xhlrpc.config.RpcConfig;
+
+import java.util.Map;
 
 /**
  * @author daiyifei
@@ -12,21 +21,62 @@ public class ConfigUtils {
 
 
     /**
-     * 加载配置对象
+     * 加载配置对象，自动识别配置文件格式
      */
     public static <T> T loadConfig(Class<T> tClass, String prefix) {
         return loadConfig(tClass, prefix, "");
     }
 
-    public  static <T> T loadConfig(Class<T> tClass, String prefix, String environment) {
+//    public  static <T> T loadConfig(Class<T> tClass, String prefix, String environment) {
+//
+//        StringBuilder configFileBuilder = new StringBuilder("application");
+//        if(StrUtil.isNotBlank(environment)){
+//            configFileBuilder.append("-").append(environment);
+//        }
+//        configFileBuilder.append(".properties");
+//        Props props = new Props(configFileBuilder.toString());
+//        System.out.println(props);
+//        return props.toBean(tClass,prefix);
+//    }
+//
+//
 
-        StringBuilder configFileBuilder = new StringBuilder("application");
-        if(StrUtil.isNotBlank(environment)){
-            configFileBuilder.append("-").append(environment);
+    public static <T> T loadConfig(Class<T> tClass, String prefix, String environment) {
+        T config = loadFromProperties(tClass, prefix, environment);
+        if (config != null) {
+            return config;
         }
-        configFileBuilder.append(".properties");
-        Props props = new Props(configFileBuilder.toString());
-        System.out.println(props);
-        return props.toBean(tClass,prefix);
+        config = loadFromYaml(tClass, prefix, environment);
+        JSONObject parse = (JSONObject) JSONUtil.parse(config);
+        T bean = JSONUtil.toBean(parse.getJSONObject(prefix), tClass);
+        System.out.println(bean);
+        return (T) bean;
+    }
+
+    private static <T> T loadFromProperties(Class<T> tClass, String prefix, String environment) {
+        String baseName = "application" + (StrUtil.isNotBlank(environment) ? "-" + environment : "") + ".properties";
+        if (FileUtil.exist(baseName)) {
+            Props props = new Props(baseName);
+            System.out.println(props);
+            return props.toBean(tClass, prefix);
+        }
+        return null;
+    }
+
+    private static <T> T loadFromYaml(Class<T> tClass, String prefix, String environment) {
+        String baseName = "application" + (StrUtil.isNotBlank(environment) ? "-" + environment : "") + ".yaml";
+        if (FileUtil.exist(baseName)) {
+            // 使用Hutool的YamlUtil读取YAML文件
+            System.out.println("Loading YAML configuration from " + baseName);
+            // 加载YAML文件为Map
+            Map<String, Object> yamlMap = YamlUtil.loadByPath(baseName);
+            JSONObject jsonObject =
+                    JSONUtil.parseObj(yamlMap);
+
+            System.out.println("jsonObject:" + jsonObject);
+            return (T) jsonObject;
+        }
+
+        return null;
     }
 }
